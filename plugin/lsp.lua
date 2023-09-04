be479f00ecaaf7d062d68781008ff2fc25f32c53
aikooo7 <prozinhopro1973@gmail.com>
2023-09-04 11:31:48 +0100

    Added some plugins.
    Signed-off-by: aikooo7 <prozinhopro1973@gmail.com>

---
+++ plugin/lsp.lua
@@ -0,0 +1,35 @@
+local lsp = require("lsp-zero")
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+    'tsserver',
+    'rust_analyzer',
+})
+lsp.set_preferences({
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+require('lspconfig').luau_lsp.setup({
+    root_dir = require('lspconfig.util').root_pattern('.git'),
+    filetypes = {'luau'}
+})
+
+lsp.setup()
+
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = {
+      ["<CR>"] = cmp.mapping.confirm({select = true})
+  }
+})
---
+++ plugin/tabs.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<Tab>", vim.cmd.BufferPrevious)
+vim.keymap.set("n", "<S-Tab>", vim.cmd.BufferNext)
+vim.keymap.set("n", "<leader>x", vim.cmd.BufferClose)
