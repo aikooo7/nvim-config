d20bb8c98782d95cd4c2e1d8484f7d3d19b13ba9
aikooo7 <prozinhopro1973@gmail.com>
2023-09-21 23:30:33 +0100

    Added a bunch of keybinds to lsp-zero.
    Signed-off-by: aikooo7 <prozinhopro1973@gmail.com>

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,7 +1,13 @@
 local lsp = require("lsp-zero")
 
-lsp.on_attach(function(bufnr)
-    lsp.default_keymaps({buffer = bufnr})
+lsp.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opbs)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 end)
 
 lsp.preset("recommended")
@@ -9,14 +15,6 @@ lsp.preset("recommended")
 lsp.ensure_installed({
     'tsserver',
     'rust_analyzer'
-})
-lsp.set_preferences({
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
 })
 
 -- Fix Undefined global 'vim'
@@ -30,6 +28,14 @@ lsp.configure('lua-language-server', {
     }
 })
 
+lsp.set_preferences({
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
 
 require('lspconfig').tsserver.setup {
     init_options = {
@@ -38,6 +44,8 @@ require('lspconfig').tsserver.setup {
         }
     }
 }
+
+-- Keybinds
 
 lsp.setup()
 
