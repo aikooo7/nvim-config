769032bc852ebcb561040a1019658b5619b6a20f
aikooo7 <prozinhopro1973@gmail.com>
2023-09-21 23:43:29 +0100

    Changed to null ls config as in the prettier.nvim.
    Signed-off-by: aikooo7 <prozinhopro1973@gmail.com>

--- plugin/null-ls.lua
+++ plugin/null-ls.lua
@@ -1,27 +1,27 @@
-local augroup = vim.api.nvim_create_augroup("Lspformatting", {})
 local null_ls = require("null-ls")
 
-local opts = {
-  sources = {
-    null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.formatting.prettier,
-  },
-  on_attach = function (client, bufnr)
+local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+local event = "BufWritePre" -- or "BufWritePost"
+local async = event == "BufWritePost"
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({
-        group = augroup,
-        buffer = bufnr
-      })
-      vim.api.nvim_create_autocmds("BufWritePre", {
-        group = augroup,
-        bufnr = bufnr,
-        callback = function ()
-          vim.lsp.buf.format({bufnr = bufnr})
+      vim.keymap.set("n", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+
+      -- format on save
+      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+      vim.api.nvim_create_autocmd(event, {
+        buffer = bufnr,
+        group = group,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr, async = async })
         end,
+        desc = "[lsp] format on save",
       })
     end
-  end,
-}
-
-return opts
+  end
+})
 
