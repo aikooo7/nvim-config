d0ca5c15c86a194a72604af2141dac8e44a28be4
aikooo7 <prozinhopro1973@gmail.com>
2023-09-25 22:47:24 +0100

    Feature: Horizontal and vertical git splitting

--- plugin/fugitive.lua
+++ plugin/fugitive.lua
@@ -15,3 +15,7 @@ end, opts)
 
 -- Git push -u is very usefull
 vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+
+-- Horizontal and vertical git splitting
+vim.keymap.set("n", "<leader>gv", vim.cmd.Gvdiffsplit)
+vim.keymap.set("n", "<leader>gh", vim.cmd.Ghdiffsplit)
