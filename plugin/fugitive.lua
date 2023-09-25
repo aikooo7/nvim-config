cedf2e28184134f30a4316b809c497251b3706ca
aikooo7 <prozinhopro1973@gmail.com>
2023-09-25 22:36:10 +0100

    Added normal push, rebase and -u push.

--- plugin/fugitive.lua
+++ plugin/fugitive.lua
@@ -1,1 +1,17 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+-- Normal push
+local bufnr = vim.api.nvim_get_current_buf()
+local opts = {buffer = bufnr, remap = false}
+vim.keymap.set("n", "<leader>p", function()
+  vim.cmd.Git('push')
+end, opts)
+
+-- rebase always
+vim.keymap.set("n", "<leader>P", function()
+  vim.cmd.Git({'pull',  '--rebase'})
+end, opts)
+
+
+-- Git push -u is very usefull
+vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
