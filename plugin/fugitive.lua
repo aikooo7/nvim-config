182367fb056fa5a47e9094474c897613302118fb
aikooo7 <prozinhopro1973@gmail.com>
2023-09-27 21:44:06 +0100

    impr: Fixed normal push keybind.

--- plugin/fugitive.lua
+++ plugin/fugitive.lua
@@ -3,9 +3,7 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 -- Normal push
 local bufnr = vim.api.nvim_get_current_buf()
 local opts = {buffer = bufnr, remap = false}
-vim.keymap.set("n", "<leader>p", function()
-  vim.cmd.Git('push')
-end, opts)
+vim.keymap.set("n", "<leader>p", ":Git push")
 
 -- rebase always
 vim.keymap.set("n", "<leader>P", ":Git push --rebase")
