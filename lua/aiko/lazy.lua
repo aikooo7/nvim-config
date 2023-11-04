fe969b950893b0be9e8ce063f820fd9252b22559
aikooo7 <prozinhopro1973@gmail.com>
2023-11-04 21:50:13 +0000

    chore: removing duplicate nvim-lspconfig.

--- lua/aiko/lazy.lua
+++ lua/aiko/lazy.lua
@@ -71,16 +71,12 @@ require("lazy").setup({
 		  {'williamboman/mason.nvim'},
 		  {'williamboman/mason-lspconfig.nvim'},
 
-      -- Easier configuration
-      {'neovim/nvim-lspconfig'},
-
 		  -- Autocompletion
 		  {'hrsh7th/nvim-cmp'},
 		  {'hrsh7th/cmp-buffer'},
 		  {'hrsh7th/cmp-path'},
 		  {'saadparwaiz1/cmp_luasnip'},
 		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
 
 		  -- Snippets
 		  {'L3MON4D3/LuaSnip'},
