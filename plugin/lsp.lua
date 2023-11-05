fcdb8d5cc8a904f0d7a9680b1a91bb7e188d920f
aikooo7 <prozinhopro1973@gmail.com>
2023-11-05 17:19:22 +0000

    fix: typo "opbs" corrected to "opts".

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -3,7 +3,7 @@ local lsp = require("lsp-zero")
 lsp.on_attach(function(_, bufnr)
     local opts = {buffer = bufnr, remap = false}
 
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opbs)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
