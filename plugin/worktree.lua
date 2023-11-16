66ec4e9517c1c5ab53d31cad4de50a461ea22ceb
aikooo7 <prozinhopro1973@gmail.com>
2023-11-16 23:15:55 +0000

    feat: removes extension so it is added in telescope.lua file

--- plugin/worktree.lua
+++ plugin/worktree.lua
@@ -2,7 +2,5 @@ require('git-worktree').setup()
 
 -- Telescope stuff
 
-require('telescope').load_extension('git_worktree')
-
 vim.keymap.set("n", "<Leader>wk", "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", silent)
 vim.keymap.set("n", "<Leader>cwk", "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>", silent)
