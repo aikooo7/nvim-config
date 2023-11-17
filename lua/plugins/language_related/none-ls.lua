24e18fe2330a38a7d5b0b3156ce5dee270d40c1b
aikooo7 <prozinhopro1973@gmail.com>
2023-11-17 22:11:39 +0000

    feat: gitsigns and code actions keybind

--- lua/plugins/language_related/none-ls.lua
+++ lua/plugins/language_related/none-ls.lua
@@ -1,30 +1,39 @@
 return {
-  "nvimtools/none-ls.nvim",
-  event = { "BufEnter", "BufWritePost" },
-  opts = function()
-    local augroup = vim.api.nvim_create_augroup("none-ls", {})
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.diagnostics.eslint_d,
-      },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({
-            group = augroup,
-            buffer = bufnr,
-          })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-          })
-        end
-      end,
-    })
-  end,
+	"nvimtools/none-ls.nvim",
+	event = { "BufEnter", "BufWritePost" },
+	keys = {
+		{
+			"<leader>ca",
+			function()
+				vim.lsp.buf.code_action()
+			end,
+		},
+	},
+	opts = function()
+		local augroup = vim.api.nvim_create_augroup("none-ls", {})
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.code_actions.gitsigns,
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({
+						group = augroup,
+						buffer = bufnr,
+					})
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
