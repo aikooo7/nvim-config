110ed93e8dfb935532ce3c2ff8b9f9f886f945d4
aikooo7 <prozinhopro1973@gmail.com>
2023-11-17 23:20:12 +0000

    perf: make gitsigns.nvim be lazy loaded

--- lua/plugins/git/git-signs.lua
+++ lua/plugins/git/git-signs.lua
@@ -1,12 +1,14 @@
 return {
-  'lewis6991/gitsigns.nvim',
+  "lewis6991/gitsigns.nvim",
+  event = { "BufEnter" },
   opts = {
     signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    }
-  }
+      add = { text = "+" },
+      change = { text = "~" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+    },
+    current_line_blame = true,
+  },
 }
