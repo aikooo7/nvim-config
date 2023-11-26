e7dddd19ce48d539276b952545159e8d0133acd8
aikooo7 <prozinhopro1973@gmail.com>
2023-11-26 01:33:22 +0000

    feat(keybinds): add keybinds for dealing with buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,9 @@
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- keybinds for buffers
+
+vim.keymap.set("n", "<Tab>", vim.cmd.bnext)
+vim.keymap.set("n", "<S-Tab>", vim.cmd.bprevious)
+vim.keymap.set("n", "<leader>x", vim.cmd.bd)
+
