b511a41412208014e01d7056f1416c73bbb6ac57
aikooo7 <prozinhopro1973@gmail.com>
2023-11-26 01:32:53 +0000

    feat(keybinds): add keybinds for moving select hunks of text

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Moves selected area also indenting
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
