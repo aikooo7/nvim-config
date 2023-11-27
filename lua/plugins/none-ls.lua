29a0d1f335add0323263ba6f75ffe9a7135370a7
aikooo7 <prozinhopro1973@gmail.com>
2023-11-27 15:25:31 +0000

    feat(plugins): add clang_format

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -15,6 +15,7 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.clang_format,
         null_ls.builtins.formatting.prettierd.with({
           disabled_filetypes = { "lua" },
         }),
