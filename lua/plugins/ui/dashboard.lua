0c1c77ace02c133ce9163754dad3b32166541d63
aikooo7 <prozinhopro1973@gmail.com>
2023-12-16 23:12:33 +0000

    feat(dashboard): switch theme from "hyper" to "doom"

--- lua/plugins/ui/dashboard.lua
+++ lua/plugins/ui/dashboard.lua
@@ -1,9 +1,11 @@
 return {
 	"nvimdev/dashboard-nvim",
 	event = "VimEnter",
+	opts = {
+		theme = "doom",
+	},
 	config = function(_, opts)
-		require("dashboard").setup({ opts })
+		require("dashboard").setup(opts)
 	end,
 	dependencies = { { "nvim-tree/nvim-web-devicons" } },
-	opts = {},
 }
