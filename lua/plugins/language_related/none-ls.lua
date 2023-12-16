addfcc1081ea2a04c3c401116de9a8f749d91447
aikooo7 <prozinhopro1973@gmail.com>
2023-12-16 15:36:34 +0000

    feat(plugins): add mason-null-ls to automatically setup up null ls with mason

--- lua/plugins/language_related/none-ls.lua
+++ lua/plugins/language_related/none-ls.lua
@@ -1,28 +1,24 @@
 return {
-	"nvimtools/none-ls.nvim",
-	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-	keys = {
-		{
-			"<leader>ca",
-			function()
-				vim.lsp.buf.code_action()
-			end,
-		},
+	"jay-babu/mason-null-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"williamboman/mason.nvim",
+		"nvimtools/none-ls.nvim",
 	},
+
 	opts = function()
+		require("mason").setup()
+		require("mason-null-ls").setup({
+			handlers = {},
+		})
 		local augroup = vim.api.nvim_create_augroup("none-ls", {})
 		local null_ls = require("null-ls")
 		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettierd.with({
-					disabled_filetypes = { "lua" },
-				}),
-				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.diagnostics.typos,
-				null_ls.builtins.diagnostics.write_good,
-				null_ls.builtins.code_actions.gitsigns,
-			},
+
+			on_init = function(new_client, _)
+				new_client.offset_encoding = "utf-16"
+			end,
+
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
 					vim.api.nvim_clear_autocmds({
