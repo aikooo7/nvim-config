931a363e1d48b82aa580ced67dacd7e41a565441
aikooo7 <prozinhopro1973@gmail.com>
2024-01-01 01:24:12 +0000

    feat(lualine): make lualine look better
    - Add sections:
      - Filename
      - diagnostics
      - Encoding
      - Filetype
      - Progress
      - Location
    
    - Add extension:
      - Fugitive

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -9,19 +9,43 @@ return {
 				section_separators = "",
 			},
 			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, -- displays file status (readonly status, modified status)
+						path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
+					},
+				},
 				lualine_x = {
 					{
-						-- require("noice").api.statusline.mode.get,
-						-- cond = require("noice").api.statusline.mode.has,
-						-- color = { fg = "#ff9e64" },
+						"diagnostics",
+						sources = { "nvim_diagnostic" },
+						symbols = { error = " ", warn = " ", info = " ", hint = " " },
 					},
+					"encoding",
+					"filetype",
 				},
-				lualine_a = {
-					{
-						"buffers",
-					},
-				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
 			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, -- displays file status (readonly status, modified status)
+						path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+					},
+				},
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			extensions = { "fugitive" },
 		})
 	end,
 }
