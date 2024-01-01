eea206dff7f522f42e22978487527cbb8cdb5d48
aikooo7 <prozinhopro1973@gmail.com>
2024-01-01 01:23:25 +0000

    feat(colorscheme): change from gruvbox to gruvbox-material

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,9 +1,10 @@
 return {
 	{
-		"morhetz/gruvbox",
+		"sainnhe/gruvbox-material",
 		lazy = false,
 		config = function()
-			vim.cmd.colorscheme("gruvbox")
+      vim.g.gruvbox_material_better_performance = 1
+			vim.cmd.colorscheme("gruvbox-material")
 		end,
 	},
 }
