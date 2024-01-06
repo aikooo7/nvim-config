64cf2696d5a9fccd700d0c229b5b9c8ee919943c
aikooo7 <prozihopro1973@gmail.com>
2024-01-06 19:45:58 +0000

    Revert "feat(plugins): add mason-null-ls to automatically setup up null ls with mason"
    This reverts commit addfcc1081ea2a04c3c401116de9a8f749d91447.

--- lua/plugins/language_related/none-ls.lua
+++ lua/plugins/language_related/none-ls.lua
@@ -1,48 +1,43 @@
 return {
-  "jay-babu/mason-null-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "williamboman/mason.nvim",
-    "nvimtools/none-ls.nvim",
-  },
-
-  opts = function()
-    local null_ls = require("null-ls")
-    require("mason").setup()
-    require("mason-null-ls").setup({
-      handlers = {
-        prettierd = function()
-          null_ls.register(null_ls.builtins.formatting.prettierd.with({
-            disabled_filetypes = { "lua", "javascript", "typescript", "json" },
-          }))
-        end,
-        deno_fmt = function()
-          null_ls.deregister(null_ls.builtins.formatting.deno_fmt)
-        end,
-      },
-    })
-    local augroup = vim.api.nvim_create_augroup("none-ls", {})
-    null_ls.setup({
-
-      on_init = function(new_client, _)
-        new_client.offset_encoding = "utf-16"
-      end,
-
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({
-            group = augroup,
-            buffer = bufnr,
-          })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-          })
-        end
-      end,
-    })
-  end,
+	"nvimtools/none-ls.nvim",
+	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+	keys = {
+		{
+			"<leader>ca",
+			function()
+				vim.lsp.buf.code_action()
+			end,
+		},
+	},
+	opts = function()
+		local augroup = vim.api.nvim_create_augroup("none-ls", {})
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettierd.with({
+					disabled_filetypes = { "lua" },
+				}),
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.diagnostics.typos,
+				null_ls.builtins.diagnostics.write_good,
+				null_ls.builtins.code_actions.gitsigns,
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({
+						group = augroup,
+						buffer = bufnr,
+					})
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
