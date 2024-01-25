6641f679b6ecd96fe44f11496df4a39fe169f999
aikooo7 <prozinhopro1973@gmail.com>
2024-01-25 17:15:14 +0000

    feat(rust): add rust.vim
    - Can auto-format with rustfmt

--- lua/plugins/language_related/rust.lua
+++ lua/plugins/language_related/rust.lua
@@ -4,4 +4,11 @@ return {
     version = "^3", -- Recommended
     ft = { "rust" },
   },
+  {
+    "rust-lang/rust.vim",
+    ft = { "rust" },
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
 }
