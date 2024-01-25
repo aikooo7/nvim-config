43e3d1d09108d8c504554d07375be49d20e50384
aikooo7 <prozinhopro1973@gmail.com>
2024-01-25 17:11:04 +0000

    feat(telescope): remove very lazy event

--- lua/plugins/navigation/telescope.lua
+++ lua/plugins/navigation/telescope.lua
@@ -1,7 +1,6 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    event = "VeryLazy",
     tag = "0.1.2",
     -- or                              , branch = '0.1.x',
     dependencies = { "nvim-lua/plenary.nvim" },
